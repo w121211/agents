92f87de15664579b029ccc88b836670ed102b2db
chi <w121210@hotmail.com>
2020-01-05 15:19:20 +0700

    .

--- mysrc/ppo_example.py
+++ mysrc/ppo_example.py
@@ -1,18 +1,3 @@
-# coding=utf-8
-# Copyright 2018 The TF-Agents Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 r"""Train and Eval PPO.
 
 To run:
@@ -25,17 +10,10 @@ python tf_agents/agents/ppo/examples/v2/train_eval.py \
   --logtostderr
 ```
 """
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import os
 import time
 
-from absl import app
-from absl import flags
-from absl import logging
+from absl import app, flags, logging
 
 import gin
 import tensorflow as tf
@@ -52,7 +30,7 @@ from tf_agents.utils import common
 from tf_agents.specs import tensor_spec
 from tf_agents.trajectories import time_step as ts, trajectory
 
-from canvas_env import CanvasEnv, DummyActorNet, DummyValueNet
+from .canvas_env import CanvasEnv
 
 
 flags.DEFINE_string('root_dir', os.getenv('TEST_UNDECLARED_OUTPUTS_DIR'),
